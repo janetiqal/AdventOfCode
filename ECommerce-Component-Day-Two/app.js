10c667a6976b0dd437c8c6f9f8974dbfa3f483b1
Janet <j.iqal35@gmail.com>
2022-01-07 17:11:03 -0600

    adding item to cart functional and increasing or decreasing count of item is functional, need to figure out the subtotals and total price

--- ECommerce-Component-Day-Two/app.js
+++ ECommerce-Component-Day-Two/app.js
@@ -1,4 +1,5 @@
 const menu = document.querySelector('ul.menu')
+const cart = document.querySelector('.cart-summary')
 
 const menuItems = [
     {
@@ -48,26 +49,91 @@ const menuItems = [
 //To Do
 
 //render menu dynamically using JS not static html
-function renderMenu(){
-    const menuItemsString = menuItems.map((item)=>{
+const renderMenu = () => {
+    const menuItemsString = menuItems.map((item, index) => {
         //toFixed returns a string but using the urnary operator makes it a Number
-        const dollars = +((item.price /100).toFixed(2))
-        // const dollars = Math.floor(item.price/100)
+        const dollars = +((item.price / 100).toFixed(2))
         // console.log(typeof dollars)
+        // const dollars = Math.floor(item.price/100)
         // const cents = (item.price % 100)
         // console.log(dollars, cents)
-        return ` <li>
+
+        //check to see if item is in the cart already, if so change the add to cart button to In Cart Btn
+        const inCart = item.count > 0
+        return `
+         <li>
         <div class="plate">
           <img src="images/${item.image}" alt="${item.alt}" class="plate" />
         </div>
         <div class="content">
           <p class="menu-item">${item.name}</p>
           <p class="price">$${dollars}</p>
-          <button class="add" onClick=(console.log("click"))>Add to Cart</button>
+          ${!inCart ? `  <button class="add" onClick=addToCart(${index})>Add to Cart</` : `<button class="in-cart" >
+          <img src="images/check.svg" alt="Check" />
+          In Cart
+      </button>` }
         </div>
       </li>`
-    })
-    menu.innerHTML= menuItemsString
+    }).join('')
+    menu.innerHTML = menuItemsString;
 }
-    // same for cart items
+// same for cart items
+const renderCart = () => {
+    
+    const cartItemsString = menuItems.map((item, index) => {
+        if(item.count === 0) return '';
+        const dollars = +((item.price / 100).toFixed(2))
+        const totalDollars = +(((item.price * item.count) /100).toFixed(2))
+        console.log(totalDollars)
+        return`
+         <li>
+        <div class="plate">
+          <img src="images/${item.image}" alt="${item.alt}"  />
+          <div class="quantity">${item.count}</div>
+        </div>
+        <div class="content">
+          <p class="menu-item">${item.name}</p>
+          <p class="price">$${dollars}</p>
+        </div>
+        <div class="quantity__wrapper">
+        <button class="decrease" onClick = decreaseCountOfItem(${index})>
+          <img src="images/chevron.svg" />
+        </button>
+        <div class="quantity">${item.count}</div>
+        <button class="increase" onClick =increaseCountOfItem(${index})>
+          <img src="images/chevron.svg" />
+        </button>
+      </div>
+        <div class="subtotal">
+          $${dollars}
+        </div>
+      </li> `
+    }
+    ).join('')
+    cart.innerHTML = cartItemsString;
+    console.log(cart)
+    
+}
+
+const decreaseCountOfItem = (i) => {
+    menuItems[i].count--;
+    //call renderCart function to update the cart otherwise if user hits count 0 it stays in the cart
+    renderCart();
+    if (menuItems[i].count === 0) {
+        renderMenu()
+    }
+}
+const increaseCountOfItem = (i) => {
+    menuItems[i].count++
+    renderCart();
+
+}
+const addToCart = (i) => {
+    //when clicked, count goes up by one
+    menuItems[i].count++
+    console.log(menuItems[i])
+    renderCart();
+    renderMenu();
+}
+
 renderMenu()
