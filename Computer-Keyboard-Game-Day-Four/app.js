a9c94a4f099417631e3d92fe61e101e4176733f3
Janet <j.iqal35@gmail.com>
2022-01-12 15:21:37 -0600

    wrote a setRandomKey function that generates a random key on the keyboard and adds the class 'jiggle', need to check if input is equal to current key jiggling

--- Computer-Keyboard-Game-Day-Four/app.js
+++ Computer-Keyboard-Game-Day-Four/app.js
@@ -1,4 +1,3 @@
-//To Do
 // grab reference to every key on the page and put it inan array 
 const keyboardBtns =[...document.querySelectorAll('.key')];
 console.log(keyboardBtns)
@@ -6,4 +5,28 @@ console.log(keyboardBtns)
 const keys = keyboardBtns.map(keyButton => keyButton.dataset.key);
 console.log(keys)
 
-//to DO determine which key is being pressed with a 'keyup' event
+
+//determine which key is being pressed with a 'keyup' event
+let currentKey;
+document.body.addEventListener('keyup',(e)=>{
+    console.log(e.key)
+    const pressedKey = e.key
+});
+
+//TO DO:
+//create a function that checks if the pressed key is === to the jiggling key
+
+
+//create a function that chooses a random key, and adds the jiggle class to it and removes the class from the previous jiggling key
+function setRandomKey(random){
+    //create a random index based on the length of the keys array and create a random key based on that random index
+
+    var randomIndex = Math.floor(Math.random() * keys.length)
+    currentKey= keys[randomIndex]
+    console.log('randomKey',currentKey)
+    // add class 'jiggle' to the randomKey
+    keyboardBtns[randomIndex].classList.add('jiggle')
+};
+
+
+setRandomKey();
