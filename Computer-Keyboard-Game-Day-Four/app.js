45e10b79c4120847eb2be9f39ec4ed6990e8d8e1
Janet <j.iqal35@gmail.com>
2022-01-12 16:34:23 -0600

    added checkKeyPressed function that checks if current key is equal to the pressed key, err with removing class of jiggle on previous key

--- Computer-Keyboard-Game-Day-Four/app.js
+++ Computer-Keyboard-Game-Day-Four/app.js
@@ -3,7 +3,6 @@ const keyboardBtns =[...document.querySelectorAll('.key')];
 console.log(keyboardBtns)
 //give vale to each key using its dataset-key value
 const keys = keyboardBtns.map(keyButton => keyButton.dataset.key);
-console.log(keys)
 
 
 //determine which key is being pressed with a 'keyup' event
@@ -11,20 +10,29 @@ let currentKey;
 document.body.addEventListener('keyup',(e)=>{
     console.log(e.key)
     const pressedKey = e.key
+    checkKeyPressed(pressedKey)
 });
 
-//TO DO:
 //create a function that checks if the pressed key is === to the jiggling key
-
+function checkKeyPressed(keyPressed){
+    if(currentKey === keyPressed.toUpperCase() ){
+        console.log('match')
+        setRandomKey()
+    }
+}
 
 //create a function that chooses a random key, and adds the jiggle class to it and removes the class from the previous jiggling key
-function setRandomKey(random){
+function setRandomKey(){
+    var randomIndex = Math.floor(Math.random() * keys.length)
+    
+    //removing prev jiggling key is NOT WORKING
+    if(currentKey){
+        keyboardBtns[randomIndex].classList.remove('jiggle')
+    }
     //create a random index based on the length of the keys array and create a random key based on that random index
-
     var randomIndex = Math.floor(Math.random() * keys.length)
     currentKey= keys[randomIndex]
-    console.log('randomKey',currentKey)
-    // add class 'jiggle' to the randomKey
+    //adding the class jiggle to the new random key
     keyboardBtns[randomIndex].classList.add('jiggle')
 };
 
