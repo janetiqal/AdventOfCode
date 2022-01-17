4d94ccf259f810a5f415b2add4a59fd1000cd11c
Janet <j.iqal35@gmail.com>
2022-01-17 16:02:42 -0600

    added a formatDollar helper function thst gives the dollar amount w two decimal places

--- RangeSlider-Day-Six/app.js
+++ RangeSlider-Day-Six/app.js
@@ -2,10 +2,13 @@ const sliderValue = document.querySelector('#priceRange')
 const outputValue = document.querySelector('.dollars')
 
 //displays the default slider value
-outputValue.innerHTML= sliderValue.value
+outputValue.innerHTML= formatDollarAmt(sliderValue.value)
 
-
+function formatDollarAmt(input){
+    // price is in cents, 
+    const formatDollar =(input /100).toFixed(2)
+    return formatDollar;   
+}
 sliderValue.oninput= function(){
-    console.log('slider is being moved')
-    outputValue.innerHTML = sliderValue.value
+    outputValue.innerHTML = formatDollarAmt(sliderValue.value)
 }
