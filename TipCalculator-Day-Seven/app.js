76353db4cfbb21153a919c554f8f3c0a398ffe15
Janet <j.iqal35@gmail.com>
2022-01-24 18:37:33 -0600

    created a var that holds the tip percentage when a radio button is selected and the calucalte btton is clicked

--- TipCalculator-Day-Seven/app.js
+++ TipCalculator-Day-Seven/app.js
@@ -7,4 +7,11 @@ console.log(calculateButton);
 
 calculateButton.addEventListener('click', function(){
     console.log('btn clicked');
+    //get the value of the tip percentage and the bill amount
+    const billTotal = billAmount.value
+    const tipButton= document.querySelector('input[name="tip"]:checked')
+    //removing the % that is attached the value of the tip using slice and turning the string to a num
+    const tipPercentage= Number(tipButton.value.slice(0,-1)/100)
+    console.log(tipPercentage);
+
 })
