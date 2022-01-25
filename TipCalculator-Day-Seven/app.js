7e0b50ec5367d876adbb2ae4526460cc9562bf9d
Janet <j.iqal35@gmail.com>
2022-01-24 18:21:15 -0600

    got a reference to every element on the page, and set up structure of calculate button event listener

---
+++ TipCalculator-Day-Seven/app.js
@@ -0,0 +1,10 @@
+const billAmount =document.querySelector('#bill-amount')
+const numOfPplDining = document.querySelector('#number-of-people')
+const tipAmt =document.querySelector('#tip-amount')
+const tipPerPerson =document.querySelector('#total-per-person')
+const calculateButton = document.querySelector('#calculate')
+console.log(calculateButton);
+
+calculateButton.addEventListener('click', function(){
+    console.log('btn clicked');
+})
