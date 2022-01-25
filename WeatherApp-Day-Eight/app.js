336630a4b6dd81cc906018cb269cf86b52685e26
Janet <j.iqal35@gmail.com>
2022-01-24 21:04:17 -0600

    added to-do for updating ui

--- WeatherApp-Day-Eight/app.js
+++ WeatherApp-Day-Eight/app.js
@@ -1,3 +1,5 @@
+const daysElements = [...document.querySelectorAll('.day')]
+
 const daysOfWeekMap = {
     0: 'SUN', 
     1: 'MON', 
@@ -22,12 +24,47 @@ const apiKey='6845bd9653c312c4a4d4b0a988d5d986'
 
 const getWeatherData= async ()=>{
     try{
-        const response =await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        const response =await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
         const data = await response.json()
         console.log(data);
+        const returnData = data.daily.map(dayWeather =>{
+            let weatherDescription="sunny";
+            console.log(dayWeather);
+            if(dayWeather.weather[0].main==='Clear'){
+                weatherDescription='clear'
+            }else if(dayWeather.weather[0].main==='Cloud'){
+                weatherDescription='cloudy'
+            }else if(dayWeather.weather[0].main==='Rain'){
+                weatherDescription='rainy'
+            }else if(dayWeather.weather[0].main==='Snow'){
+                weatherDescription='snowy'
+            }
+            //creating an object with all the relevant data we want for each day in a week
+            const objectData = {
+                minTemp: Math.round(dayWeather.temp.min),
+                maxTemp: Math.round(dayWeather.temp.max),
+                dayTemp: Math.round(dayWeather.temp.day),
+                pop: dayWeather.pop * 100 ,
+                weatherDescription
+            }
+            console.log(objectData);
+            return objectData
+        })
+        console.log(returnData);
+
+        return returnData
     }
     catch(error){
         console.log(error)
     }
 }
+//toDO
+
+//CHANGE THE UI based on the data coming 
+const updateUI= (data)=>{
+console.log('heu',daysElements);
+    //loop through the day elements and set up for mapping of weather data
+}
+
+updateUI()
   getWeatherData()
