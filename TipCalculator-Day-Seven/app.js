5a0b8c1eb7f72968ce18b7a8bc19d72af326ab0a
Janet <j.iqal35@gmail.com>
2022-01-24 18:56:14 -0600

    calculated tip amount and total per person with inner html updating

--- TipCalculator-Day-Seven/app.js
+++ TipCalculator-Day-Seven/app.js
@@ -3,15 +3,17 @@ const numOfPplDining = document.querySelector('#number-of-people')
 const tipAmt =document.querySelector('#tip-amount')
 const tipPerPerson =document.querySelector('#total-per-person')
 const calculateButton = document.querySelector('#calculate')
-console.log(calculateButton);
 
 calculateButton.addEventListener('click', function(){
     console.log('btn clicked');
     //get the value of the tip percentage and the bill amount
-    const billTotal = billAmount.value
+    let billTotal = Number(billAmount.value)
     const tipButton= document.querySelector('input[name="tip"]:checked')
     //removing the % that is attached the value of the tip using slice and turning the string to a num
     const tipPercentage= Number(tipButton.value.slice(0,-1)/100)
-    console.log(tipPercentage);
+    const tipTotal =(billTotal*tipPercentage).toFixed(2)
 
+    tipPerPerson.innerHTML= (tipTotal /numOfPplDining.value).toFixed(2)
+    tipAmt.innerHTML= tipTotal;
 })
+
